deece2ab784d6848c99c9adb2c6c9692553329b7
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-23 15:38:40 +0900

    init s3 action

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.7-alpine
+
+LABEL "com.github.actions.name"="S3 Sync"
+LABEL "com.github.actions.description"="Sync a Directory to a S3 repository"
+LABEL "com.github.actions.icon"="refresh-cw"
+LABEL "com.github.actions.color"="green"
+
+LABEL version="0.0.1"
+LABEL repository="https://github.com/awact/s3-action"
+LABEL homepage="https://github.com/awact/s3-action"
+LABEL maintainer="Shun Kakinoki @shunkakinoki"
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    ca-certificates \
+    git \
+    && apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV WORKDIR /app/
+
+WORKDIR ${WORKDIR}
+
+RUN pip install --upgrade pip && pip install pipenv
+
+RUN pipenv install --system --ignore-pipfile --deploy
+
+ADD entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+  echo "AWS_REGION is not set. Quitting."
+  exit 1
+fi
+
+# Default to CLI defined AWS endpoint
+ENDPOINT_APPEND=""
+if [ "$AWS_S3_ENDPOINT" ]; then
+  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
+fi
+
+# Default to syncing entire repo if SOURCE_DIR not set.
+SOURCE_DIR=${SOURCE_DIR:-.}
+
+# Create a dedicated profile for this action to avoid
+# conflicts with other actions.
+# https://github.com/jakejarvis/s3-sync-action/issues/1
+aws configure --profile s3-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Use our dedicated profile and suppress verbose messages.
+# All other flags are optional via `args:` directive.
+sh -c "aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET} \
+              --profile s3-sync-action \
+              --no-progress $*"
